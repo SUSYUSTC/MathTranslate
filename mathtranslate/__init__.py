db40f8bca6ef4e2c2c92c99320238b605d6cae14
Jiace Sun <susyustc@gmail.com>
2023-03-22 03:00:00 -0700

    update

--- mathtranslate/__init__.py
+++ mathtranslate/__init__.py
@@ -4,6 +4,5 @@ __author__ = "Jiace Sun"
 import os
 ROOT = os.path.dirname(os.path.abspath(__file__))
 from . import config
-from . import tencent
 from . import translation
 from .translation import translate
