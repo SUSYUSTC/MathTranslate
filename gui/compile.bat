dc6c7c1f162bafa19635d21c08c71d97bc93c163
Jiace Sun <susyustc@gmail.com>
2023-06-23 17:30:20 -0700

    add font to compile

--- gui/compile.bat
+++ gui/compile.bat
@@ -1,2 +1,2 @@
-pyi-makespec --onefile --noconsole --add-data ".\guipage\page.kv;.\guipage" --add-data ".\guipage\dialog.kv;.\guipage" --add-data ".\guipage\preferencespage.kv;.\guipage" --hidden-import win32timezone MathTranslate.py
+pyi-makespec --onefile --noconsole --add-data ".\guipage\page.kv;.\guipage" --add-data ".\guipage\dialog.kv;.\guipage" --add-data ".\guipage\preferencespage.kv;.\guipage" --add-data ".\yahei.ttf;.\" --hidden-import win32timezone MathTranslate.py
 pyinstaller MathTranslate.spec
--- gui/compile.sh
+++ gui/compile.sh
@@ -1,2 +1,2 @@
-pyi-makespec --onefile --noconsole --add-data ./guipage/page.kv:./guipage --add-data ./guipage/dialog.kv:./guipage --add-data ./guipage/preferencespage.kv:./guipage MathTranslate.py
+pyi-makespec --onefile --noconsole --add-data ./guipage/page.kv:./guipage --add-data ./guipage/dialog.kv:./guipage --add-data ./guipage/preferencespage.kv:./guipage --add-data ./yahei.ttf:./ MathTranslate.py
 pyinstaller MathTranslate.spec
