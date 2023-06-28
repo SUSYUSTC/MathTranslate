158d79a964ec4f1c6a43adc50a1a56590d2bf71d
Jiace Sun <susyustc@gmail.com>
2023-06-28 16:38:25 -0700

    update

--- gui/update.py
+++ gui/update.py
@@ -15,7 +15,7 @@ def update_package(os_name):
     open(zippath, 'wb').write(r.content)
 
     with zipfile.ZipFile(zippath, 'r') as zip_ref:
-        zip_ref.extractall('MathTranslate', members=filter(lambda x: x.filename[0:6] != 'update', zip_ref.infolist()))
+        zip_ref.extractall('./', members=filter(lambda x: x.filename[0:6] != 'update', zip_ref.infolist()))
     os.remove(zippath)
 
 
@@ -24,4 +24,5 @@ if __name__ == '__main__':
     raw_name = platform.system()
     os_name = os_names[raw_name]
     update_package(os_name)
+    print('Update Finished')
     input('Press Enter to exit')
