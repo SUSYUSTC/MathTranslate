4e59b5c78c1910a13e72c5ea81b329a017358fe3
Jiace Sun <susyustc@gmail.com>
2023-08-15 21:58:02 -0700

    update

--- gui/compile.bat
+++ gui/compile.bat
@@ -2,11 +2,11 @@ if exist ".\build" (
 	rm -r build
 )
 pyi-makespec --onefile --noconsole --add-data ".\guipage\page.kv;.\guipage" --add-data ".\guipage\dialog.kv;.\guipage" --add-data ".\guipage\preferencespage.kv;.\guipage" --add-data ".\yahei.ttf;." --hidden-import win32timezone MathTranslate.py
-pyinstaller MathTranslate.spec
+pyinstaller MathTranslate.spec | findstr /V /C:"DEBUG" | findstr /V /C:"TRACE"
 
 if exist ".\build" (
 	rm -r build
 )
 pyi-makespec --onefile update.py
-pyinstaller update.spec
+pyinstaller update.spec | findstr /V /C:"DEBUG" | findstr /V /C:"TRACE"
 
