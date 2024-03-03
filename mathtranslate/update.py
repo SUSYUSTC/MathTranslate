c2b32a7f43c2dee95b2a132b81d03b540c950f69
Zipper-1 <gongzhangpeng42@gmail.com>
2024-03-03 23:34:57 +0800

    Add timeout for updater
    Add a timeout for checking update so users having poor connectivity with pypi.org won't keep waiting FOREVER

--- mathtranslate/update.py
+++ mathtranslate/update.py
@@ -5,7 +5,7 @@ import urllib.request
 def get_latest_version():
     url = 'https://pypi.org/pypi/mathtranslate/json'
 
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(url,timeout=15) as response:
         data = json.loads(response.read().decode('utf-8'))
         latest_version = data['info']['version']
 
